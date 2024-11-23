a4bc26e1e33dc2c04e3bcd3bd98958bbdbc0366f
s4abap41 <s4abap41@gmail.com>
2024-11-24 02:38:30 +0530

    sss

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,12 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("project1.controller.View1", {
-        onInit: function () {
+        return Controller.extend("project1.controller.View1", {
+            onInit: function () {
+                var oMydata = new sap.ui.model.json.JSONModel();
+                oMydata.loadData("./data/student.json","", false);
+                debugger;
+                console.log(JSON.stringify(oMydata.getData()));
 
-        }
+                var oInput = this.getView().byId("inp1");
+                oInput.setModel(oMydata);
+
+            }
+        });
     });
-});
